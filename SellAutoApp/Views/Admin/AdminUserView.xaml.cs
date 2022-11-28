6c91b3a2569174d2f2aeb48035c644deb788f7cd
Andrey <morgunof-andrey@yandex.ru>
2022-11-28 08:18:23 +0300

    Add enter view

---
+++ SellAutoApp/Views/Admin/AdminUserView.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using SellAutoApp.DataAccess;
+using SellAutoApp.Models;
+
+namespace SellAutoApp.Views.Admin;
+
+public partial class AdminUserView : Window, INotifyPropertyChanged
+{
+    private ObservableCollection<User> users;
+
+    public ObservableCollection<User> Users
+    {
+        get => users;
+        set
+        {
+            users = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public AdminUserView()
+    {
+        InitializeComponent();
+        DataContext = this;
+        LoadData();
+    }
+
+    private void LoadData()
+    {
+        using var context = new SellCarDbContext();
+        Users = new ObservableCollection<User>(context.Users.ToList());
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+
+    private void Exit_OnClick(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+}
---
+++ SellAutoApp/Views/EnterView.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using SellAutoApp.DataAccess;
+using SellAutoApp.Views.Admin;
+
+namespace SellAutoApp.Views;
+
+public partial class EnterView : Window {
+    public EnterView() {
+        InitializeComponent();
+    }
+
+    private void Login_OnClick(object sender, RoutedEventArgs e) {
+        TryLogin();
+    }
+
+    private void Exit_OnClick(object sender, RoutedEventArgs e) {
+        Application.Current.Shutdown();
+    }
+
+    private void TryLogin() {
+        try {
+            using var context = new SellCarDbContext();
+            var user = context.Users
+                .Include(x => x.UserRole)
+                .FirstOrDefault(x => x.UserName == login.Text);
+            if (user != null) {
+                var generatedHash = GenerateHash(Encoding.UTF8.GetBytes(password.Password));
+                var hash = Convert.FromBase64String(user.UserHash);
+                if (!CompareByteArrays(generatedHash, hash)) {
+                    MessageBox.Show("Неправильный пароль");
+                    return;
+                }
+
+                switch (user.UserRole.UserRoleName)
+                {
+                    case "Admin":
+                        var adminUserView = new AdminUserView();
+                        adminUserView.Show();
+                        Close();
+                        break;
+                    case "Client":
+                        break;
+                    default:
+                        throw new Exception("Unknown role type");
+                }
+
+            } else {
+                MessageBox.Show("Такого пользователя не существует");
+            }
+        } catch (Exception ex) {
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+    private static byte[] GenerateHash(byte[] password) {
+        var algorithm = new SHA256Managed();
+
+        var plainTextWithSaltBytes = new byte[password.Length];
+
+        for (var i = 0; i < password.Length; i++) {
+            plainTextWithSaltBytes[i] = password[i];
+        }
+
+        return algorithm.ComputeHash(plainTextWithSaltBytes);
+    }
+
+    private static bool CompareByteArrays(byte[] array1, byte[] array2) {
+        if (array1.Length != array2.Length) {
+            return false;
+        }
+
+        return !array1.Where((t, i) => t != array2[i]).Any();
+    }
+}
